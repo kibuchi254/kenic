40c519cf02c3d131c98f72fa147ecadeb0143ab1
ELIJAH KIBUCHI <info.lijohtechdevelopers@gmail.com>
2025-08-14 23:39:23 +0300

    feefef

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 3005,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  server: {
+    host: "::",
+    port: 3005,
+    allowedHosts: ['digikenya.co.ke'],
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
