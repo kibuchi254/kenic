f68e3cc212aa4b045c96a4556f6cb59517a773c7
ELIJAH KIBUCHI <info.lijohtechdevelopers@gmail.com>
2025-08-15 09:33:21 +0300

    dfefeieef

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // Server configuration for development
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 3005,
     allowedHosts: ['digikenya.co.ke'],
   },
-  // Plugins to be used by Vite
   plugins: [
     react(),
-    // Use component tagger only in development mode
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
-  // Path aliases for cleaner imports
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
