92deda3cc67915b34ba03341849b709790f89b4c
ELIJAH KIBUCHI <info.lijohtechdevelopers@gmail.com>
2025-08-25 12:11:58 +0300

    Integration with WHMCS

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/context/AuthContext"; // Import AuthProvider
 import Index from "./pages/Index";
 import DomainCheckout from "./pages/DomainCheckout";
 import Signin from "./pages/Signin";
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Signup from "./pages/Signup";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,20 +18,21 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/domain-checkout" element={<DomainCheckout />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/signup" element={<Signup />} />
-          {/* Add other routes here */}
-          
-          {/* Catch-all route for 404 Not Found */}
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider> {/* Wrap BrowserRouter with AuthProvider */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/domain-checkout" element={<DomainCheckout />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* Add other routes here */}
+
+            {/* Catch-all route for 404 Not Found */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
